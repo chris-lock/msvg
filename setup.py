7ba9637f29d75fdae0fb3b4821788b813683859c
Chris Lock <lock.christopher@gmail.com>
2015-07-21 00:11:42 -0400

    Making msg a module that can be installed.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name = 'msvg',
+	packages = ['msvg'],
+	package_data = {},
+	entry_points = {
+		'console_scripts': ['msvg = msvg.msvg:main']
+	},
+	version = '0.1.0',
+	description = 'A Python parsing library for http://www.openstreetmap.org/ SVG files.',
+	long_description = open('README.md', 'r').read(),
+	author = 'Chris Lock',
+	author_email = 'chris@bright.is',
+	url = 'https://github.com/chris-lock/msvg',
+)
